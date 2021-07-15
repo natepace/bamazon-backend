6e122d6976bea3bae71a5d3cf573a5b3d1b74141
natepace <n8wpace@gmail.com>
2021-07-15 17:46:43 -0600

    heroku ts 5

--- api/server.js
+++ api/server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const helmet = require('helmet')
+const cors = require('cors')
 const usersRouter = require('./users/users-router.js');
 const productsRouter = require('./products/products-router.js');
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors())
 server.use('/users', usersRouter);
 server.use('/products', productsRouter)
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "up" })
+})
 
 server.use((err, req, res, next) => { // eslint-disable-line
     res.status(500).json({
